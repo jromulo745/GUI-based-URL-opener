9ceaea0605f126c9f73940818941e5adb56aba91
jromulo745 <85465170+jromulo745@users.noreply.github.com>
2022-05-25 20:47:44 -0700

    Configure LinkOpener to extend JFrame and implement ActionListener

--- LinkOpener.java
+++ LinkOpener.java
@@ -1,6 +1,8 @@
 import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class LinkOpener
+public class LinkOpener extends JFrame implements ActionListener
 {
     public static void main(String[] args)
     {
@@ -9,5 +11,11 @@ public class LinkOpener
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(450, 450);
         frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        
     }
 }
