32b69575aa8967723a7e5db0c8905c2158432cc6
jromulo745 <85465170+jromulo745@users.noreply.github.com>
2022-05-25 20:30:00 -0700

    Create an initial frame

--- LinkOpener.java
+++ LinkOpener.java
@@ -1,8 +1,13 @@
+import javax.swing.JFrame;
 
 public class LinkOpener
 {
     public static void main(String[] args)
     {
-        
+        JFrame frame = new JFrame();
+        frame.setTitle("Link Opener");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(450, 450);
+        frame.setVisible(true);
     }
 }
